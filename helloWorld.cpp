9078bfcbb14ad0d8b9facb6f9ab00e2a959c4e62
keshiamh9 <54156059+keshiamh9@users.noreply.github.com>
2019-08-19 10:44:03 -0700

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,15 @@
+//Programmer: Keshia Horton
+//Purpose: Hello World
+//Date Modified: 4/3/19
+//Compiler: MS VC++ Community
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World! \n";
+	system("pause");
+	return 0;
+
+}
